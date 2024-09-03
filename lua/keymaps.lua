f74e56bf90195e61c12336458b955d6598fccfc3
Cody Melton <cmelton@sandia.gov>
2024-09-03 14:36:49 -0600

    add splits

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,6 +1,10 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- use similar bindings to tmux
+vim.keymap.set('n', '<leader>%', ':vsplit<CR>', { desc = 'vsplit' })
+vim.keymap.set('n', '<leader>"', ':split<CR>', { desc = 'split' })
+
 vim.keymap.set('n', '<leader>ex', ':Explore<CR>', { desc = "e[x]plore" } )
 vim.keymap.set('n', '<leader>es', ':Sexplore<CR>', { desc = "[s]plit explore"} )
 vim.keymap.set('n', '<leader>ev', ':Vexplore<CR>', { desc = "[v]split expore"} )
